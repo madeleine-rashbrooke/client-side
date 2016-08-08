66c1caffc280c70944a4a0b37dea2ac9348eb325
Madeleine <email": null>
2016-08-09 11:38:41 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 var xhr = require('xhr')
-var example = require('./views/example.hbs')
+var greeting = require('./views/greeting.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
-  if (err) console.log(err) // do something
+var endpoint = 'https://api.wheretheiss.at/v1/satellites'
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+xhr.get(endpoint, function (err, data) {
+  if (err) {
+    console.error(err)
+  }
+
+  // In case you're curious
+  console.log(data.body) // FYI: data.body is a string
+
+  // Replace 'Space' below with the response
+  var target = document.getElementsByTagName('main')[0]
+
+
+  var satObj = JSON.parse(data.body)
+  // console.log("this is satObj: ", satObj)
+  var satName = satObj[0].name
+  var satPosition = satObj[0].id
+  console.log("this is satName: ", satName)
+  target.innerHTML = greeting({name: satName, position: satPosition})
+  //target.innerHTML = greeting({name: 'Space Thing'})
 })
-
